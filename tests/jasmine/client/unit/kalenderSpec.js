ed5036bc4e39195964285ce49dc3a1686d078f84
Kristel <kristel.tammesalu@gmail.com>
2014-10-13 21:23:41 +0300

    Added jasmine package for unit testing

---
+++ tests/jasmine/client/unit/kalenderSpec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kristel on 13.10.14.
+ */
+/*describe('dateExists', function() {
+    it("should return true if date exists", function() {
+        var cursor = {
+            count: function () {
+                return 1;
+            }
+        };
+        spyOn(Lectures, 'find').and.returnValue(cursor);
+        expect(selectedDate.dateExists()).toBe(true);
+    });
+});*/
